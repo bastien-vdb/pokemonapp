136e54d33aedcbcae97ebe26a2656a8ce586b270
bastien-vdb <bastien.deboisrolin@gmail.com>
2022-10-19 10:38:38 +0200

    css flexbox added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Cards from './components/Cards';
+
+const cs = console.log;
 
 function App() {
+  useEffect(()=>{
+    const apiCall = async() => {
+      const result = await fetch('https://pokeapi.co/api/v2/pokemon/').then(async(res)=>{
+        res = await res.json();
+        return res.results;
+      })
+      result.forEach(element=>{
+        fetch(element.url)
+          .then(res=>{
+            res.json()
+            .then(res=>{
+              cs(res);
+            })
+          })
+      })
+    }
+    apiCall();
+  },[])
+  
   return (
-    <div>
-      Hello
+    <div className=''>
+      <Cards/>
     </div>
   );
 }
---
+++ src/Cardsff.js
@@ -0,0 +1,32 @@
+
+export default function Cards() {
+    return (
+        <div>
+            <div class=" bg-indigo-50">
+                <section class="min-h-screen body-font text-gray-600 ">
+                    <div class="container mx-auto px-5 py-10">
+                        <div class="-m-4 flex flex-wrap">
+                            <div class="w-full p-4 md:w-1/2 lg:w-1/4">
+                                <a class="relative block h-48 overflow-hidden rounded">
+                                    <img alt="ecommerce" class="block h-full w-full object-cover object-center cursor-pointer" src="https://dummyimage.com/420x260" />
+                                </a>
+                                <div class="mt-4">
+                                    <h3 class="title-font mb-1 text-xs tracking-widest text-gray-500">PROJECT</h3>
+                                    <h2 class="title-font text-lg font-medium text-gray-900">The Catalyzer</h2>
+                                    <p class="mt-1">01/09/2022</p>
+                                </div>
+                            </div>
+                            
+                           
+                            
+                            
+                        </div>
+                    </div>
+                </section>
+                <footer>
+                    <p class="text-center  py-4 bg-white">Create By &copy; Bastien VDB</p>
+                </footer>
+            </div>
+        </div>
+    )
+}
