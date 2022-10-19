fffa834de8d9ddc4bd1d0a25740a37bcaf36885f
bastien-vdb <bastien.deboisrolin@gmail.com>
2022-10-19 19:36:17 +0200

    first API call inserted and display result

--- src/App.js
+++ src/App.js
@@ -6,28 +6,45 @@ import Cards from './components/Cards';
 const cs = console.log;
 
 function App() {
-  useEffect(()=>{
-    const apiCall = async() => {
-      const result = await fetch('https://pokeapi.co/api/v2/pokemon/').then(async(res)=>{
+
+  //useState
+  const [pokemonGo, setpokemonGo] = useState([]);
+
+  useEffect(() => {
+
+    //1. Poke API Call
+    const apiCall = async () => {
+      //Using of async function + then() only for the json transformation
+      const result = await fetch('https://pokeapi.co/api/v2/pokemon/').then(async (res) => {
         res = await res.json();
         return res.results;
       })
-      result.forEach(element=>{
+
+      //2. Using of all URL retrieved before to fetch again (API Call) and insert the requested data into an object for each, and in a Array for All
+      result.forEach(element => {
         fetch(element.url)
-          .then(res=>{
+          .then(res => {
             res.json()
-            .then(res=>{
-              cs(res);
-            })
+              .then(res => {
+                const obj = { picture: res.sprites.other.dream_world.front_default, name: res.name, type: res.types[0].type.name, ability: res.abilities[0].ability.name }
+                cs(obj)
+                //3. Insertion of the requested data into a corresponding useState to be showing on screen
+                setpokemonGo((e) => [...e, obj]);
+              })
           })
       })
+
+      await cs(pokemonGo);
     }
     apiCall();
-  },[])
-  
+  }, [])
+
   return (
-    <div className=''>
-      <Cards/>
+    <div>
+    {/* Simple container for flexbox using: Each box got a fixed size to force the wrap */}
+      <div className='h-full w-fit m-10 flex gap-5 gap-y-14 flex-wrap justify-around'>
+        {pokemonGo.map((e, key) => <Cards id={key} picture={e.picture} name={e.name} type={e.type} ability={e.ability} />)}
+      </div>
     </div>
   );
 }
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,56 +1,21 @@
 import React from 'react';
 
-function Cards(props) {
+const Cards = ({ picture, name, type, ability, key }) => {
     return (
         <div>
-            {/* Simple container for flexbox using: Each box got a fixed size to force the wrap */}
-            <div id='container' className='h-full w-fit m-10 flex gap-5 gap-y-14 flex-wrap justify-around'>
+            {/* Inside the flexbox container: Each box got a fixed size to force the wrap */}
                 <div id='box' className=' h-full w-[500px] rounded-lg flex flex-col items-center p-2 border-2 border-red-100 shadow-2xl'>
                     <div id='picture' className='h-5/6 m-1 rounded-sm'>
-                        <img className='h-full w-96' src='example.svg' alt='pic example'/>
+                        <img className='h-full w-96' src={picture} alt='pic example' />
                     </div>
                     <div id='description' className='p-2 text-center'>
-                        <p className='font-bold uppercase'>Name</p>
-                        <p>Type</p>
-                        <p>Color</p>
-                    </div>
-                    <button className='w-fit rounded-lg border-2 border-black shadow-2xl p-2 hover:scale-110 transition duration-300 ease-in-out'>more...</button>
-                </div>
-                <div id='box' className=' h-full w-[500px] rounded-lg flex flex-col items-center p-2 border-2 border-red-100 shadow-2xl'>
-                    <div id='picture' className='h-5/6 m-1 rounded-sm'>
-                        <img className='h-full w-96' src='example.svg' alt='pic example'/>
-                    </div>
-                    <div id='description' className='p-2 text-center'>
-                        <p className='font-bold uppercase'>Name</p>
-                        <p>Type</p>
-                        <p>Color</p>
-                    </div>
-                    <button className='w-fit rounded-lg border-2 border-black shadow-2xl p-2 hover:scale-110 transition duration-300 ease-in-out'>more...</button>
-                </div>
-                <div id='box' className=' h-full w-[500px] rounded-lg flex flex-col items-center p-2 border-2 border-red-100 shadow-2xl'>
-                    <div id='picture' className='h-5/6 m-1 rounded-sm'>
-                        <img className='h-full w-96' src='example.svg' alt='pic example'/>
-                    </div>
-                    <div id='description' className='p-2 text-center'>
-                        <p className='font-bold uppercase'>Name</p>
-                        <p>Type</p>
-                        <p>Color</p>
-                    </div>
-                    <button className='w-fit rounded-lg border-2 border-black shadow-2xl p-2 hover:scale-110 transition duration-300 ease-in-out'>more...</button>
-                </div>
-                <div id='box' className=' h-full w-[500px] rounded-lg flex flex-col items-center p-2 border-2 border-red-100 shadow-2xl'>
-                    <div id='picture' className='h-5/6 m-1 rounded-sm'>
-                        <img className='h-full w-96' src='example.svg' alt='pic example'/>
-                    </div>
-                    <div id='description' className='p-2 text-center'>
-                        <p className='font-bold uppercase'>Name</p>
-                        <p>Type</p>
-                        <p>Color</p>
+                        <p className='font-bold uppercase'>{name? name:'wait'}</p>
+                        <p className='capitalize'>Type: {type? type:'wait'}</p>
+                        <p className='capitalize'>Ability: {ability? ability:'wait'}</p>
                     </div>
                     <button className='w-fit rounded-lg border-2 border-black shadow-2xl p-2 hover:scale-110 transition duration-300 ease-in-out'>more...</button>
                 </div>
             </div>
-        </div>
     );
 }
 
