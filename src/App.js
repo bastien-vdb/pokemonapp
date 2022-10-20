d7617105144b624a61537477d9177fc12020facf
bastien-vdb <bastien.deboisrolin@gmail.com>
2022-10-20 07:05:54 +0200

    responsivity with cards resizing breackpoint

--- src/App.js
+++ src/App.js
@@ -9,41 +9,49 @@ function App() {
 
   //useState
   const [pokemonGo, setpokemonGo] = useState([]);
+  const [getNextUrl, setNextUrl] = useState('https://pokeapi.co/api/v2/pokemon/');
+
+  //1. Poke API Call
+  const apiCall = async () => {
+    //Using of async function + then() only for the json transformation
+    const result = await fetch(getNextUrl).then(async (res) => {
+      res = await res.json();
+      setNextUrl(() => res.next);
+      return res.results;
+    })
+
+    //2. Using of all URL retrieved before to fetch again (API Call) and insert the requested data into an object for each, and in a Array for All
+    result.forEach(element => {
+      fetch(element.url)
+        .then(res => {
+          res.json()
+            .then(res => {
+              const obj = { picture: res.sprites.other.dream_world.front_default, name: res.name, type: res.types[0].type.name, ability: res.abilities[0].ability.name, version:res.game_indices[0].version.name, weight:res.weight}
+              cs(obj)
+              //3. Insertion of the requested data into a corresponding useState to be showing on screen
+              cs(res)
+              setpokemonGo((e) => [...e, obj]);
+            })
+        })
+    })
+  }
 
   useEffect(() => {
-
-    //1. Poke API Call
-    const apiCall = async () => {
-      //Using of async function + then() only for the json transformation
-      const result = await fetch('https://pokeapi.co/api/v2/pokemon/').then(async (res) => {
-        res = await res.json();
-        return res.results;
-      })
-
-      //2. Using of all URL retrieved before to fetch again (API Call) and insert the requested data into an object for each, and in a Array for All
-      result.forEach(element => {
-        fetch(element.url)
-          .then(res => {
-            res.json()
-              .then(res => {
-                const obj = { picture: res.sprites.other.dream_world.front_default, name: res.name, type: res.types[0].type.name, ability: res.abilities[0].ability.name }
-                cs(obj)
-                //3. Insertion of the requested data into a corresponding useState to be showing on screen
-                setpokemonGo((e) => [...e, obj]);
-              })
-          })
-      })
-
-      await cs(pokemonGo);
-    }
     apiCall();
   }, [])
 
   return (
     <div>
-    {/* Simple container for flexbox using: Each box got a fixed size to force the wrap */}
-      <div className='h-full w-fit m-10 flex gap-5 gap-y-14 flex-wrap justify-around'>
-        {pokemonGo.map((e, key) => <Cards id={key} picture={e.picture} name={e.name} type={e.type} ability={e.ability} />)}
+      <div className='flex flex-col items-center'>
+        {/* Simple container for flexbox using: Each box got a fixed size to force the wrap */}
+        <div className='h-full w-fit m-10 flex gap-5 gap-y-14 flex-wrap justify-around'>
+          {pokemonGo.map((e, key) => <Cards {...e}  />)}
+        </div>
+
+        <div className='m-4'>
+          <button className='w-fit font-bold bg-black text-white rounded-lg border-2 border-black shadow-2xl p-2 hover:scale-110 transition duration-300 ease-in-out' onClick={() => apiCall()}>More...</button>
+        </div>
+
       </div>
     </div>
   );
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Cards = ({ picture, name, type, ability, key }) => {
+const Cards = ({ picture, name, type, ability, key, version, weight }) => {
+    const [more, setMore] = useState(false);
     return (
         <div>
             {/* Inside the flexbox container: Each box got a fixed size to force the wrap */}
-                <div id='box' className=' h-full w-[500px] rounded-lg flex flex-col items-center p-2 border-2 border-red-100 shadow-2xl'>
-                    <div id='picture' className='h-5/6 m-1 rounded-sm'>
-                        <img className='h-full w-96' src={picture} alt='pic example' />
-                    </div>
-                    <div id='description' className='p-2 text-center'>
-                        <p className='font-bold uppercase'>{name? name:'wait'}</p>
-                        <p className='capitalize'>Type: {type? type:'wait'}</p>
-                        <p className='capitalize'>Ability: {ability? ability:'wait'}</p>
-                    </div>
-                    <button className='w-fit rounded-lg border-2 border-black shadow-2xl p-2 hover:scale-110 transition duration-300 ease-in-out'>more...</button>
+            <div id='box' className=' h-full w-[200px] md:w-[300px] lg:w-[500px] rounded-lg flex flex-col items-center p-2 border-2 border-red-100 shadow-2xl hover:scale-110 transition duration-300 ease-in'>
+                <div id='picture' className='h-5/6 m-1 rounded-sm'>
+                    {picture ? <img className='h-full w-96' src={picture} alt='pic example' /> : 'Loading picture...'}
                 </div>
+                <div id='description' className='p-2 text-center'>
+                    <div className='capitalize'>
+                        <p className='font-bold upperca se'>{name ? name : 'wait'}</p>
+                        <p>Type: {type ? type : 'wait'}</p>
+                        <p>Ability: {ability ? ability : 'wait'}</p>
+                    </div>
+                    <div style={{ display: more ? 'block' : 'none' }} className='capitalize transition duration-300'>
+                        <p>Weight: {weight}</p>
+                        <p>Version: {version}</p>
+                    </div>
+                </div>
+                <button onClick={() => setMore(() => !more)} className='w-fit rounded-lg border-2 border-black shadow-2xl p-2 hover:scale-110 transition duration-300 ease-in-out'>{more? 'Less...':'More...'}</button>
             </div>
+        </div>
     );
 }
 
